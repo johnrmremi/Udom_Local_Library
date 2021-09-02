ce6a9884e2ad4c068b554fc588947438ab402ecb
johnrmremi <johnrmremi01@gmail.com>
2021-09-02 11:19:21 +0300

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-v-_c6m%6v0n21y
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['hidden-cliffs-26636.herokuapp.com']
+ALLOWED_HOSTS = ['hidden-cliffs-26636.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
